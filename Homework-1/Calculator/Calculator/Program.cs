a697e7d49087afbee85d6ca8af6cbd9882034472
Szymon <szymon.malitka@yahoo.pl>
2023-11-23 12:45:44 +0100

    Added function to validate input types

--- Homework-1/Calculator/Calculator/Program.cs
+++ Homework-1/Calculator/Calculator/Program.cs
@@ -3,29 +3,68 @@
 
     public class Calculator
     {
-        public double Add(double a, double b) => a + b;
-        public double Substract(double a, double b) => a - b;
-        public double Multiply(double a, double b) => a * b;        
-        public double Divide(double a, double b) => a / b;        
+        public  double Add(double a, double b) => a + b;
+        public  double Substract(double a, double b) => a - b;
+        public  double Multiply(double a, double b) => a * b;        
+        public  double Divide(double a, double b) => a / b;        
 
-        public double Exponent(double a, double b) => (double)Math.Pow(a, b);
+        public  double Exponent(double a, double b) => (double)Math.Pow(a, b);
 
-        public long Factorial(long a) => a == 0 ? 1 : a * Factorial(a - 1);
+        public long Factorial (int n) => n == 0 ? 1 : n * Factorial(n - 1);
+
+        public void PrintResult(double result) => Console.WriteLine(result);
+
+        public void PrintResult(long result) => Console.WriteLine(result);
+
+        public bool ValidateNumbers(params string[] numbers)
+        {
+            foreach (var number in numbers)
+            {
+                if (!double.TryParse(number, out _))
+                {
+                    Console.WriteLine("Invalid number");
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        
+        
+
+        public void Start()
+        {   
+
+
+            while (true)
+            {
+                Console.WriteLine("Select the operation (+, -, *, /, !,^, Exit):");
+                string operation = Console.ReadLine();
+                if (operation == "Exit")
+                {
+                    break;
+                }        
+
+               
+
+
+            }
+         
+
+
+      
+        }   
 
 
     
-
-        public void PrintResult(double result)
-        {
-            Console.WriteLine(result);
-        }
-
 
         
     }
     private static void Main(string[] args)
     {
-        Calculator myCalc = new Calculator();
-        Console.WriteLine(myCalc.Add(4, 4));
+       Calculator myCalc = new Calculator();
+        myCalc.Start();
+        
+        
     }
 }
