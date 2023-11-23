06b22dd6c6aa63698585bad444bf050c00b280a6
Szymon <szymon.malitka@yahoo.pl>
2023-11-23 10:44:41 +0100

    Added Calculator class with math methods

--- Homework-1/Calculator/Calculator/Program.cs
+++ Homework-1/Calculator/Calculator/Program.cs
@@ -1,5 +1,44 @@
 ﻿internal class Program
 {
+
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Substract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            return a / b;
+
+        }
+
+        public int Exponent(int a, int b)
+        {
+            return a ^ b;
+        }
+
+        public long Factorial(int a)
+        {
+            long result = 1;
+            for (int i = 1; i <= a; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
