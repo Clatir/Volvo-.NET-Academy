40e9e57aa5580af8930fbf181e899c34b41a305b
Szymon <szymon.malitka@yahoo.pl>
2023-11-23 20:05:08 +0100

    Added sample results

--- Homework-1/Calculator/Calculator/Program.cs
+++ Homework-1/Calculator/Calculator/Program.cs
@@ -1,24 +1,31 @@
-﻿internal class Program
+﻿using System.Reflection.Metadata.Ecma335;
+
+internal class Program
 {
 
     public class Calculator
     {
-        public  double Add(double a, double b) => a + b;
-        public  double Substract(double a, double b) => a - b;
-        public double Multiply(double a, double b) => a * b;
-        public double Divide(double a, double b)
-        {
-            if (b != 0) return a / b;
-            return 0;
+        public  static double Add(double a, double b) => Math.Round(a + b, 3);
+        public  static double Substract(double a, double b) => Math.Round(a - b,3);
+        public static double Multiply(double a, double b) => Math.Round(a * b, 3);
+        public static double Divide(double a, double b)
+        { 
+            if(b== 0)
+            {
+                return 0;
+            }
+            
+            return Math.Round( a / b,3);
         }
 
-        public double Exponent(double a, double b) => (double)Math.Pow(a, b);
+        
+        public static double Exponent(double a, double b) => Math.Round((double)Math.Pow(a, b),3);
 
-        public long Factorial (int n) => n == 0 ? 1 : n * Factorial(n - 1);
+        public static long Factorial (int n) => n == 0 ? 1 : n * Factorial(n - 1);
 
-        public void PrintResult(double result) => Console.WriteLine(result);
+        public static void PrintResult(double result) => Console.WriteLine(result);
 
-        public void PrintResult(long result) => Console.WriteLine(result);
+        public static void PrintResult(long result) => Console.WriteLine(result);
 
         public bool ValidateNumbers(params string[] numbers)
         {
@@ -33,6 +40,37 @@
             return true;
         }
 
+        public void SampleResults()
+        {
+            Random rnd = new Random();
+            var rndDouble = new Random();
+
+            var rDouble = rndDouble.NextDouble();
+
+
+            int rndNum = rnd.Next(1, 10);
+            int rndNum2 = rnd.Next(1, 10);
+            var rRangeDouble = Math.Round(rDouble * (10 - 1) + 1, 2);
+            var rRangeDouble2 = Math.Round(rDouble * (10 - 2) + 2, 2);
+
+            //var rRangeDouble = rDouble * (upperBound - lowerBound) + lowerBound;
+
+            Console.WriteLine("\nRandom double number between 1 and 10: " + rRangeDouble);
+
+            Console.WriteLine("Random double number2 between 2 and 10: " + rRangeDouble2);
+
+            Console.WriteLine("Random number between 1 and 10: " + rndNum);
+
+            Console.WriteLine("Sample addition: " + rRangeDouble + " + " + rRangeDouble2 + " = " + (Add(rRangeDouble, rRangeDouble2)));
+            Console.WriteLine("Sample substraction: " + rRangeDouble + " - " + rRangeDouble2 + " = " + (Substract(rRangeDouble, rRangeDouble2)));
+            Console.WriteLine("Sample multiplication: " + rRangeDouble + " * " + rRangeDouble2 + " = " + (Multiply(rRangeDouble, rRangeDouble2)));
+            Console.WriteLine("Sample division: " + rRangeDouble + " / " + rRangeDouble2 + " = " + (Divide(rRangeDouble, rRangeDouble2)));
+            Console.WriteLine("Sample exponent: " + rndNum + " ^ " + rndNum2 + " = " + (Exponent(rndNum, rndNum2)));
+            Console.WriteLine("Sample factorial: " + rndNum + " ! " + " = " + (Factorial(rndNum)));
+            
+
+        }
+
         
         
         
@@ -43,18 +81,18 @@
 
             while (true)
             {
-                Console.WriteLine("Select the operation (+, -, *, /, !,^, Exit):");
+                Console.WriteLine("\nSelect the option (+, -, *, /, !,^, Exit):\nTo show sample results type 'sample'\n");
                 string operation = Console.ReadLine();
                 if (operation == "Exit")
                 {
                     break;
                 }
-                
+
                 else if (operation == "!")
                 {
                     Console.WriteLine("Enter the number:");
                     string number = Console.ReadLine();
-                    if (int.TryParse(number, out int numberParsed) & numberParsed >=0)
+                    if (int.TryParse(number, out int numberParsed) & numberParsed >= 0)
                     {
 
 
@@ -62,10 +100,10 @@
                     }
                     else
                     { Console.WriteLine("No factorial for floats"); }
-                    
+
                 }
 
-               else if (operation == "+")
+                else if (operation == "+")
                 {
                     Console.WriteLine("Enter the first number:");
                     string firstNumber = Console.ReadLine();
@@ -100,7 +138,7 @@
                         PrintResult(Multiply(double.Parse(firstNumber), double.Parse(secondNumber)));
                     }
                 }
-               else if (operation == "/") 
+                else if (operation == "/")
                 {
                     Console.WriteLine("Enter the first number:");
                     string firstNumber = Console.ReadLine();
@@ -111,16 +149,16 @@
                         if (double.Parse(secondNumber) == 0)
                         {
                             Console.WriteLine("Cannot divide by zero");
-                            
+
                         }
 
                         PrintResult(Divide(double.Parse(firstNumber), double.Parse(secondNumber)));
                     }
-                
-                
+
+
                 }
 
-               else if(operation =="^")
+                else if (operation == "^")
                 {
                     Console.WriteLine("Enter the first number:");
                     string firstNumber = Console.ReadLine();
@@ -131,6 +169,10 @@
                         PrintResult(Exponent(double.Parse(firstNumber), double.Parse(secondNumber)));
                     }
                 }
+
+                else if (operation == "sample")
+                    SampleResults();
+
                 else
                 {
                     Console.WriteLine("Invalid operation");
@@ -153,8 +195,22 @@
     }
     private static void Main(string[] args)
     {
-       Calculator myCalc = new Calculator();
+        
+
+
+
+
+
+
+
+
+
+
+
+        Calculator myCalc = new Calculator();
         myCalc.Start();
+
+
         
         
     }
