52307ef144d2dbd73b8740913f4a4873d54b542e
Szymon <szymon.malitka@yahoo.pl>
2023-11-23 16:45:50 +0100

    Fully implemented math methods with edge cases

--- Homework-1/Calculator/Calculator/Program.cs
+++ Homework-1/Calculator/Calculator/Program.cs
@@ -5,10 +5,14 @@
     {
         public  double Add(double a, double b) => a + b;
         public  double Substract(double a, double b) => a - b;
-        public  double Multiply(double a, double b) => a * b;        
-        public  double Divide(double a, double b) => a / b;        
+        public double Multiply(double a, double b) => a * b;
+        public double Divide(double a, double b)
+        {
+            if (b != 0) return a / b;
+            return 0;
+        }
 
-        public  double Exponent(double a, double b) => (double)Math.Pow(a, b);
+        public double Exponent(double a, double b) => (double)Math.Pow(a, b);
 
         public long Factorial (int n) => n == 0 ? 1 : n * Factorial(n - 1);
 
@@ -31,6 +35,7 @@
 
         
         
+        
 
         public void Start()
         {   
@@ -43,7 +48,93 @@
                 if (operation == "Exit")
                 {
                     break;
-                }        
+                }
+                
+                else if (operation == "!")
+                {
+                    Console.WriteLine("Enter the number:");
+                    string number = Console.ReadLine();
+                    if (int.TryParse(number, out int numberParsed) & numberParsed >=0)
+                    {
+
+
+                        PrintResult(Factorial(numberParsed));
+                    }
+                    else
+                    { Console.WriteLine("No factorial for floats"); }
+                    
+                }
+
+               else if (operation == "+")
+                {
+                    Console.WriteLine("Enter the first number:");
+                    string firstNumber = Console.ReadLine();
+                    Console.WriteLine("Enter the second number:");
+                    string secondNumber = Console.ReadLine();
+                    if (ValidateNumbers(firstNumber, secondNumber))
+                    {
+                        PrintResult(Add(double.Parse(firstNumber), double.Parse(secondNumber)));
+                    }
+                }
+
+                else if (operation == "-")
+                {
+                    Console.WriteLine("Enter the first number:");
+                    string firstNumber = Console.ReadLine();
+                    Console.WriteLine("Enter the second number:");
+                    string secondNumber = Console.ReadLine();
+                    if (ValidateNumbers(firstNumber, secondNumber))
+                    {
+                        PrintResult(Substract(double.Parse(firstNumber), double.Parse(secondNumber)));
+                    }
+                }
+
+                else if (operation == "*")
+                {
+                    Console.WriteLine("Enter the first number:");
+                    string firstNumber = Console.ReadLine();
+                    Console.WriteLine("Enter the second number:");
+                    string secondNumber = Console.ReadLine();
+                    if (ValidateNumbers(firstNumber, secondNumber))
+                    {
+                        PrintResult(Multiply(double.Parse(firstNumber), double.Parse(secondNumber)));
+                    }
+                }
+               else if (operation == "/") 
+                {
+                    Console.WriteLine("Enter the first number:");
+                    string firstNumber = Console.ReadLine();
+                    Console.WriteLine("Enter the second number:");
+                    string secondNumber = Console.ReadLine();
+                    if (ValidateNumbers(firstNumber, secondNumber))
+                    {
+                        if (double.Parse(secondNumber) == 0)
+                        {
+                            Console.WriteLine("Cannot divide by zero");
+                            
+                        }
+
+                        PrintResult(Divide(double.Parse(firstNumber), double.Parse(secondNumber)));
+                    }
+                
+                
+                }
+
+               else if(operation =="^")
+                {
+                    Console.WriteLine("Enter the first number:");
+                    string firstNumber = Console.ReadLine();
+                    Console.WriteLine("Enter the second number:");
+                    string secondNumber = Console.ReadLine();
+                    if (ValidateNumbers(firstNumber, secondNumber))
+                    {
+                        PrintResult(Exponent(double.Parse(firstNumber), double.Parse(secondNumber)));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid operation");
+                }
 
                
 
