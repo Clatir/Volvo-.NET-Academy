94d9a711f05c6c24ce78ee24d4f20f8c57cf2a8c
Szymon <szymon.malitka@yahoo.pl>
2023-12-27 12:10:37 +0100

    Initial commit Homework 2

---
+++ Homework-2/Leasing-Company/Leasing-Company/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+
+{
+
+    public abstract class vehicle
+    {
+        
+        public int id { get; set; }
+        public string brand { get; set; }
+        public string model { get; set; }
+        public string yearOfProduction { get; set; }
+        public string color { get; set; }
+        public double price { get; set; }
+        public string registrationNumber { get; set; }
+        public int mileage { get; set; }
+        public int serviceInterval { get; set; }
+        public string comfortClass { get; set; }
+        public long durationOfTrip { get; set; }
+        public long travelDistance { get; set; }
+        public double modelCoefficient { get; set; }
+
+    }
+
+    class passengerVehicle : vehicle
+    {
+
+        public double lesseesRating { get; set; }
+    
+    }
+
+    class cargoVehicle : vehicle
+    {
+        public double cargoWeight { get; set; }
+    }
+
+    private static void Main(string[] args)
+    {
+
+        passengerVehicle myPassangerVehicle = new passengerVehicle();
+
+
+        Console.WriteLine("Hello, World!");
+    }
+}
